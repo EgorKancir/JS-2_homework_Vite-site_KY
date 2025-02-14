33c23bcfd24ff3c62f2c88239f7835fa86f6e5b1
EgorKancir <kancyregor@gmail.com>
2025-02-14 10:55:55 +0100

    index7: resolve(__dirname, index7.html)

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
                 index4: resolve(__dirname, "index4.html"),
                 index5: resolve(__dirname, "index5.html"),
                 index6: resolve(__dirname, "index6.html"),
-                index7: resolve(__dirname, "index7.html")
+                index7: resolve(__dirname, "index7.html"),
+                index8: resolve(__dirname, "index8.html")
             }
         }
     }
