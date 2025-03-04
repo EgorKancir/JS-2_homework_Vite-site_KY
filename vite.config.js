ff0a623b55d2f5284c566e060b9a5f62127ae46e
EgorKancir <kancyregor@gmail.com>
2025-03-04 18:07:21 +0100

    HW11

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
                 index7: resolve(__dirname, "index7.html"),
                 index8: resolve(__dirname, "index8.html"),
                 index9: resolve(__dirname, "index9.html"),
-                index10: resolve(__dirname, "index10.html")
+                index10: resolve(__dirname, "index10.html"),
+                index11: resolve(__dirname, "index11.html")
             }
         }
     }
